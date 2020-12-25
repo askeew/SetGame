b4715ad4245c8c6f659e98102ba27a8f205f3bcd
Askia <askeew@hotmail.com>
2020-12-25 09:10:59 +0100

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Askia on 2020-12-25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
